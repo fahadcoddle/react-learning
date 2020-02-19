64533084b863df18dcef4cbd94f74927d1749c2b
fahad d <fahad.d@coddletech.com>
2020-02-19 17:30:21 +0530

    initial

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = props =>{
 return(
     <div>
-        <p>I am {props.name} and i am {props.age} years old</p>
+        <p>I am {props.name} and i   am {props.age} years old</p>
         {props.children}
     </div>
 ) 
