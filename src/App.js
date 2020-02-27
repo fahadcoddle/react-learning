2bed08a2e16463476114cded306efc2a1283e8e6
fahad d <fahad.d@coddletech.com>
2020-02-27 11:22:50 +0530

    third chapter complete

--- src/App.js
+++ src/App.js
@@ -3,36 +3,65 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-
-  state = {
+   state={
     persons: [
       {name: 'John' , age: 20},
       {name: 'Steve' , age: 21},
       {name: 'Tom' , age: 22}
     ]
-  }
-  nameSwitchHandeler = () =>{
+  };
+
+   nameSwitchHandeler = (newName) =>{
     this.setState({
       persons: [
-        {name: 'John ' , age: 30},
-        {name: 'Steve ' , age: 31},
+        {name: newName , age: 30},
+        {name: 'Bravo' , age: 31},
         {name: 'Max' , age: 32}
       ]
-
+  
     })
   }
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi Welcome to the React page</h1>
-        <p>Hey i can have nested things as well !!</p>
-        <button onClick={this.nameSwitchHandeler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My hobby is dancing</Person>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
-      </div>
-    );
+
+  newNameHandeler = (event) =>{
+    this.setState({
+      persons: [
+        {name: 'Boxer' , age: 30},
+        {name: event.target.value , age: 31},
+        {name: 'Max' , age: 32}
+      ]
+    })
+
   }
+  render(){
+    const style = {
+      backgroundColor : 'white',
+      padding: '10px',
+      border: '1px solid blue',
+      cursor : 'pointer'
+    }
+  return (
+    <div className="App">
+      <h1>Hi Welcome to the React page</h1>
+      <p>Hey i can have nested things as well !!</p>
+      <button
+      style = {style} 
+      onClick={this.nameSwitchHandeler.bind(this,'aaron')}>Switch Name</button>
+      <Person 
+      name = {this.state.persons[0].name} 
+      age = {this.state.persons[0].age} />
+      <Person 
+      name = {this.state.persons[1].name} 
+      age = {this.state.persons[1].age}
+      click={this.nameSwitchHandeler.bind(this,'peter parker')}
+      newName={this.newNameHandeler}></Person>
+      <Person
+      name = {this.state.persons[2].name} 
+      age = {this.state.persons[2].age}/>
+    </div>
+  );
+}
 }
 
 export default App;
+
+
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = props =>{
 return(
-    <div>
-        <p>I am {props.name} and i   am {props.age} years old</p>
+    <div className="Person">
+        <p onClick = {props.click}>I am {props.name} and i   am {props.age} years old</p>
         {props.children}
+        <input type="text" onChange={props.newName} value={props.name} />
     </div>
 ) 
 }
